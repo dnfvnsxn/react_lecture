8067e712b451eb9b692d11ace15b0b5f1b2aabaa
dnfvnsxn <dnfvnsxn@naver.com>
2020-01-02 22:11:06 +0900

    webpack preset env setting

--- 2. WordChain/webpack.config.js
+++ 2. WordChain/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
     name: "word-chain-setting",
@@ -16,16 +17,17 @@ module.exports = {
         rules: [
             {
                 // js와 jsx 파일에 바벨을 적용하여 최신이나 실험적인 문법을 예전 문법으로 바꿔주겠다.
-                test: /\.jsx?/,
+                test: /\.jsx?$/,
                 loader: "babel-loader",
                 options: {
-                    presets: ["@babel/preset-env", "@babel/preset-react"],
+                    //plugin의 모음이 preset
+                    presets: [["@babel/preset-env", { targets: { browsers: ["> 5% in KR"] }, debug: true }], "@babel/preset-react"], // @babel/preset-env가 브라우져 지원을 설정하는 것인데 세부 설정하는 방법 //browserslist
                     plugins: ["@babel/plugin-proposal-class-properties"]
                 }
             }
         ]
     },
-
+    plugins: [new webpack.LoaderOptionsPlugin({ debug: true })],
     // 출력
     output: {
         path: path.join(__dirname, "dist"),
