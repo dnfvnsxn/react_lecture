bff8c2dcd4dea35472c4e00e782ed8fd16857d64
dnfvnsxn <dnfvnsxn@naver.com>
2020-02-07 11:38:46 +0900

    immer

--- 7. TicTacTo/hooks/TicTacToe.jsx
+++ 7. TicTacTo/hooks/TicTacToe.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useReducer, useCallback } from "react";
+import produce from "immer";
 import Table from "./Table";
 
 const initialState = {
@@ -20,38 +21,30 @@ export const RESET_GAME = "RESET_GAME";
 const reducer = (state, action) => {
     switch (action.type) {
         case SET_WINNER:
-            // state.winner = action.winner; 이렇게 하면 안됨.
-            return {
-                ...state,
-                winner: action.winner
-            };
+            return produce(state, draft => {
+                draft.winner = action.winner;
+            });
         case CLICK_CELL: {
-            const tableData = [...state.tableData];
-            tableData[action.row] = [...tableData[action.row]]; // immer라는 라이브러리로 가독성 해결
-            tableData[action.row][action.cell] = state.turn;
-            return {
-                ...state,
-                tableData,
-                recentCell: [action.row, action.cell]
-            };
+            return produce(state, draft => {
+                draft.tableData[action.row][action.cell] = state.turn;
+                draft.recentCell = [action.row, action.cell];
+            });
         }
         case CHANGE_TURN: {
-            return {
-                ...state,
-                turn: state.turn === "O" ? "X" : "O"
-            };
+            return produce(state, draft => {
+                draft.turn = state.turn === "O" ? "X" : "O";
+            });
         }
         case RESET_GAME: {
-            return {
-                ...state,
-                turn: "O",
-                tableData: [
-                    ["", "", ""],
-                    ["", "", ""],
-                    ["", "", ""]
-                ],
-                recentCell: [-1, -1]
-            };
+            return produce(state, draft => {
+                (draft.turn = "O"),
+                    (draft.tableData = [
+                        ["", "", ""],
+                        ["", "", ""],
+                        ["", "", ""]
+                    ]);
+                draft.recentCell = [-1, -1];
+            });
         }
         default:
             return state;
